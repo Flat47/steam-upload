5dfc2baeff13d62461136549d0057b4f9dc6f599
x414e54 <x414e54@linux.com>
2020-12-20 18:26:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,28 @@ try {
   const script = core.getInput('script');
   const username = core.getInput('username');
   const password = core.getInput('password');
-  const { execSync, spawn } = require('child_process');
+  const { spawn } = require('child_process');
   
   if (script.includes('\'') || script.includes('"') || username.includes('\'') || username.includes('"') || password.includes('\'') || password.includes('"'))
   {
      core.setFailed("Invalid characters in string");
   }
 
-  execSync('"C:\\actions-runner\\sdk\\tools\\ContentBuilder\\builder\\steamcmd.exe" "+login ' + username + ' ' + password + ' +run_app_build_http ' + script + ' +quit"', (err, stdout, stderr) => {
-    console.log(stdout);
-    console.log(stderr);
-    if (err) {
-      core.setFailed(err);
-      return;
-    }
+  var proc = spawn('C:\\actions-runner\\sdk\\tools\\ContentBuilder\\builder\\steamcmd.exe', ['+login', username, password, '+run_app_build_http', script, '+quit']);
+  
+  proc.stdout.on('data', function (data) {
+    console.log(data.toString());
+  });
+
+  proc.stderr.on('data', function (data) {
+    console.error(data.toString());
+  });
+
+  proc.on('error', function () {
+    console.error('Failed to launch.'); 
+  });
+
+  proc.on('exit', function () {
   });
 } catch (error) {
   core.setFailed(error.message);
